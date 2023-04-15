51ae0f01546302b4b701c1ba9fcb68f5c2a31f9c
Poltergeist <77846583+nmeisters@users.noreply.github.com>
2023-04-15 15:00:29 +0200

    update

--- download.py
+++ download.py
@@ -1,45 +1,42 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
+import requests
+import json
+import os
+import datetime
 
 
-def download(url, path):
-    # Create an instance
-    options = Options()
-    options.headless = False
-    options.binary_location = r'C:/Program Files/Mozilla Firefox/firefox.exe'
-    driver = webdriver.Firefox(executable_path=r"C:/Program Files/Mozilla Firefox/geckodriver.exe", options=options)
-    
-    try:
-        driver.get(url)
+def download(url, newspaper):
 
-        try:
-            button = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, path))
-            )
-            button.click()
-        except Exception as e:
-            print("Error:", e) 
+    if not os.path.exists("Data"):
+        os.mkdir("Data")    
+    if not os.path.exists(f"Data/{newspaper}"):
+        os.mkdir(f"Data/{newspaper}")
 
-        # Click the button
-        button.click()
+    response = requests.get(url)   
 
-        # get the HTML source code
-        html_code = driver.page_source
-        print(html_code)
-        
-        #Kill browser
-        driver.quit()
-        
-    except:
-        #driver.quit()
-        print("Error")
+    if response.status_code == 200:
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")        
+        with open(f"Data/{newspaper}/{timestamp}.html", "w", encoding="utf-8") as f:
+            f.write(response.text)
 
 
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    #download(url = "https://www.swr.de/swr2/", path ="//button[@aria-label=' abspielen']")
-    download(url="https://www.spiegel.de/", path="//button[@title='Akzeptieren und weiter']")
+    download("https://www.bild.de/", "bild")
+    download("https://welt.de", "welt")
+    download("https://www.faz.net/aktuell/", "faz")
+    download("https://www.taz.de/", "taz")
+    download("https://www.wz.de/", "westdeutsche")
+    download("https://www.sueddeutsche.de/", "sz")
+    download("https://www.zeit.de/", "zeit")
+    download("https://www.spiegel.de/", "spiegel") 
+    download("https://www.tagesschau.de/", "tagesschau")
+    download("https://www.handelsblatt.com/", "handelsblatt")
+    download("https://www.nd-aktuell.de/", "neue-deutsche")
+    download("https://www.freitag.de/", "freitag")
+    download("https://tagesspiegel.de", "tagesspiegel")
+    download("https://www.fr.de/", "fr")
+    download("https://jungefreiheit.de/", "jungefreiheit")
+    download("https://www.jungewelt.de/", "jungewelt")
+    download("https://www.merkur.de/", "merkur")
+    download("https://www.n-tv.de/", "ntv")
+    download("https://www.rp-online.de/", "rponline")
