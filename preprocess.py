5eb15939872022861350bd5f8690892059cacb4e
Poltergeist <77846583+nmeisters@users.noreply.github.com>
2023-04-15 16:48:37 +0200

    zon working

--- preprocess.py
+++ preprocess.py
@@ -15,7 +15,6 @@ def read_files(folder):
 
 
 def save_zeit_as_json(homepages):
-
     list_homepages = []    
 
     for homepage in homepages:        
@@ -26,38 +25,59 @@ def save_zeit_as_json(homepages):
 
         for article in articles:           
             #variables            
-            dict_article = {"header": "", "subheader": "", "is_main_article": False}
+            dict_article = {"header": "", "subheader": "", "type": "", "meta": ""}
             
-
             #for the main article
             header_element = article.css('.zon-teaser-classic__faux-link')
             if header_element:
                 dict_article["header"] = header_element[0].text()
-                dict_article["is_main_article"] = True
-
+                dict_article["type"] = "main"
             subheader_element = article.css('.zon-teaser-classic__text') 
             if subheader_element:
-                dict_article["subheader"] = subheader_element[0].text()              
-
+                dict_article["subheader"] = subheader_element[0].text()
+            meta_element = article.css('.zon-teaser-classic__metadata')
+            if meta_element:
+                dict_article["meta"] = meta_element[0].text()         
 
             #for the regular articles
             header_element = article.css('.zon-teaser-standard__faux-link')
             if header_element:
-                dict_article["header"] = header_element[0].text()                
-            
+                dict_article["header"] = header_element[0].text()              
             subheader_element = article.css('.zon-teaser-standard__text')
             if subheader_element:
                 dict_article["subheader"] = subheader_element[0].text()
+                dict_article["type"] = "regular"
+            meta_element = article.css('.zon-teaser-standard__metadata')
+            if meta_element:
+                dict_article["meta"] = meta_element[0].text()
+
+            #for the wide articles
+            header_element = article.css('.zon-teaser-wide__faux-link')
+            if header_element:
+                dict_article["header"] = header_element[0].text()              
+            subheader_element = article.css('.zon-teaser-wide__text')
+            if subheader_element:
+                dict_article["subheader"] = subheader_element[0].text()
+                dict_article["type"] = "wide"
+            meta_element = article.css('.zon-teaser-wide__metadata')
+            if meta_element:
+                dict_article["meta"] = meta_element[0].text()
 
 
-            #save in homepage dict
-            dict_homepage["articles"].append(dict_article)
+            if dict_article["header"] != "":
+                #save in homepage dict
+                dict_homepage["articles"].append(dict_article)
 
+
+        #Append to list of homepages
         list_homepages.append(dict_homepage)
 
-    with open("Data/zeit/zeit.jsonl", "w", encoding="utf-8") as f:
-        json.dump(list_homepages, f, indent=2)
-    
+
+    #save in json file
+    if not os.path.exists("Data/All"):
+        os.mkdir("Data/All") 
+    with open("Data/All/zeit.jsonl", "w", encoding="utf-8") as f:
+        json.dump(list_homepages, f, indent=2)    
     
                 
  
