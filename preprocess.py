d9bc04c38f9a2c0e39847794dc13f0861de020ec
Poltergeist <77846583+nmeisters@users.noreply.github.com>
2023-04-17 14:21:32 +0200

    start w/ welt

--- preprocess.py
+++ preprocess.py
@@ -27,7 +27,7 @@ def save_zeit_as_json(homepages):
             #variables            
             dict_article = {"header": "", "subheader": "", "type": "", "meta": ""}
             
-            #for the main article
+            #for the main articles
             header_element = article.css('.zon-teaser-classic__faux-link')
             if header_element:
                 dict_article["header"] = header_element[0].text()
@@ -37,7 +37,19 @@ def save_zeit_as_json(homepages):
                 dict_article["subheader"] = subheader_element[0].text()
             meta_element = article.css('.zon-teaser-classic__metadata')
             if meta_element:
-                dict_article["meta"] = meta_element[0].text()         
+                dict_article["meta"] = meta_element[0].text()     
+
+            #for the lead articles
+            header_element = article.css('.zon-teaser-lead__faux-link')
+            if header_element:
+                dict_article["header"] = header_element[0].text()
+                dict_article["type"] = "lead"
+            subheader_element = article.css('.zon-teaser-lead__text') 
+            if subheader_element:
+                dict_article["subheader"] = subheader_element[0].text()
+            meta_element = article.css('.zon-teaser-lead__metadata')
+            if meta_element:
+                dict_article["meta"] = meta_element[0].text()    
 
             #for the regular articles
             header_element = article.css('.zon-teaser-standard__faux-link')
@@ -81,7 +93,54 @@ def save_zeit_as_json(homepages):
 
 
 
+def save_welt_as_json(homepages):
+    list_homepages = []    
+
+    for homepage in homepages:        
+        dict_homepage = {"timestamp": homepage[0], "articles": []}
+        html = homepage[1]
+        html = HTMLParser(html)
+        articles = html.css('article')
+
+        for article in articles:           
+            #variables            
+            dict_article = {"topic": "", "header": "", "subheader": "", "type": "", "meta": ""}
+
+
+            #for the main articles
+            teaser_element = article.css('.o-teaser__topic')
+            if teaser_element:
+               dict_article["topic"] = teaser_element[0].text()
+
+            headline_element = article.css('.o-headline')
+            if headline_element:
+                dict_article["header"] = headline_element[0].text()
+
+            text_element = article.css('.o-text')
+            if text_element:
+                dict_article["subheader"] = text_element[0].text()
+
+
+            if dict_article["header"] != "":
+                    #save in homepage dict
+                    dict_homepage["articles"].append(dict_article) 
+        
+        #Append to list of homepages
+        list_homepages.append(dict_homepage)
+    
+    #save in json file
+    if not os.path.exists("Data/All"):
+        os.mkdir("Data/All") 
+    with open("Data/All/welt.jsonl", "w", encoding="utf-8") as f:
+        json.dump(list_homepages, f, indent=2) 
+
+
+
+
 if __name__ == "__main__":
     zeit = read_files("zeit")
     save_zeit_as_json(zeit)
+
+    welt = read_files("welt")
+    save_welt_as_json(welt)
     
